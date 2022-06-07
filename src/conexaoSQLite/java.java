d4ab919aa23dc6e04466fb30cf3e7c6a1b73f07c
Leonardo Comelli <leonardo@software-ltda.com.br>
2022-06-07 20:59:59 -0300

    first commit

---
+++ src/conexaoSQLite/java.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package conexaoSQLite;
+
+/**
+ *
+ * @author felipe
+ */
+public class java {
+    
+}
